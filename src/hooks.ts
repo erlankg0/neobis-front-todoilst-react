126bce7d7368bc93186465d6c78dbd49c6e243f3
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2024-03-31 13:35:46 +0300

    deploy project

--- src/hooks.ts
+++
@@ -1,6 +0,0 @@
-import {useDispatch, useSelector, TypedUseSelectorHook} from "react-redux";
-import type {RootState, AddDispatch} from "./redux/store.ts";
-
-
-export const useAddDispatch = () => useDispatch<AddDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import './normalize.css'
-import AppContent from "./layout/App/AppContent.tsx";
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <AppContent/>
-)
